17035f768279a0a5ff7b974127983795bf419c5b
Amirbek02 <amirbek.amiraev.kk@gmail.com>
2025-04-03 02:05:26 +0600

    [Forget] create repository

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*', // Можно указать конкретные домены вместо '*'
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
